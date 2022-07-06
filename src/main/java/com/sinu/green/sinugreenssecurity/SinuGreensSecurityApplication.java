cfb478bbdf41a1f7593dae302654acaa4bb0e2c5
srinivasa_b <srinivasa_b@USMB135143.local>
2022-07-05 18:12:39 -0700

    sample configured to keycloak authorization server

---
+++ src/main/java/com/sinu/green/sinugreenssecurity/SinuGreensSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.sinu.green.sinugreenssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinuGreensSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinuGreensSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinu/green/sinugreenssecurity/SinuGreensSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinu.green.sinugreenssecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinuGreensSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
